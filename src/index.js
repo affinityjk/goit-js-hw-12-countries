9d361a92ce2dac9d23e4eec25e03efdc366d708e
Julia Kutsyn <yuliyaflm@gmail.com>
2021-05-18 18:35:49 +0300

    adds source files and markup templates

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './sass/main.scss';
+import './sass/main.scss';
+import countries from './templates/countries.hbs';
+import countriesList from './templates/countriesList.hbs';
+import debounce from 'lodash.debounce';
+
